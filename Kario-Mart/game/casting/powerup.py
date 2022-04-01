f035eb7fa55bfce72d4a6d2bb6038d77bb0e75fa
clarkscoberly <91291906+clarkscoberly@users.noreply.github.com>
2022-04-01 17:57:48 -0600

    Add files via upload

---
+++ Kario-Mart/game/casting/powerup.py
@@ -0,0 +1,26 @@
+import random
+import constants
+from game.casting.actor import Actor
+from game.shared.point import Point
+
+
+class PowerUp(Actor):
+    """
+    A tasty item that carts like to eat.
+
+    Attributes:
+        _points (int): The number of points the food is worth.
+    """
+    def __init__(self):
+        "Constructs a new Food."
+        super().__init__()
+        self.set_text("?")
+        self.set_color(constants.RED)
+        self._action = None
+
+    def get_action(self):
+        return self._action
+    
+    def set_action(self, action):
+        self._action = action
+        
